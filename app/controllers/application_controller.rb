fbd4295fef1a66ced22d1ca4ec3ea6ae1742dff5
Remo Mueller <remosm@gmail.com>
2016-08-09 10:38:09 -0400

    Don't verify authenticity_token on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@
 
 # Main web application controller for Boston Sleep website
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :devise_login?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def devise_login?
+    params[:controller] == 'devise/sessions' && params[:action] == 'create'
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :emails_enabled])
+  end
 end
