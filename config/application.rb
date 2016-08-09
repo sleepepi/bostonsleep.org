5ee2b85923e171b6d90069d248b03428f3b622e6
Remo Mueller <remosm@gmail.com>
2016-08-09 10:28:33 -0400

    Updated rails 5 configuration files

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +23,9 @@ module BostonsleepOrg
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rails time:zones" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
